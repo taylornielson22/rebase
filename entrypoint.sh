62f9ef4d269926d63eb4e63698a84baa6803073a
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2022-09-21 12:27:58 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,7 @@ git fetch origin $BASE_BRANCH
 git fetch fork $HEAD_BRANCH
 git checkout -b fork/$HEAD_BRANCH fork/$HEAD_BRANCH
 
-if [[ $INPUT_REBASE ]]; then
+if [[ "$INPUT_REBASE" == "true" ]]; then
 	if [[ $INPUT_AUTOSQUASH -eq 'true' ]]; then
 		GIT_SEQUENCE_EDITOR=: git rebase -i --autosquash origin/$BASE_BRANCH
 	else
@@ -72,7 +72,7 @@ if [[ $INPUT_REBASE ]]; then
 	git push --force-with-lease fork fork/$HEAD_BRANCH:$HEAD_BRANCH
 fi
 
-if [[ $INPUT_MERGE ]]; then
+if [[ "$INPUT_MERGE" == "true" ]]; then
 	git checkout -b origin/$BASE_BRANCH origin/$BASE_BRANCH
 	git merge fork/$HEAD_BRANCH --no-commit
 	git push --force-with-lease origin origin/$BASE_BRANCH:$BASE_BRANCH
